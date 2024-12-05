c8d0bb10ba638e34e7e0de6df5ccd79fd34e55e0
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2024-12-05 16:14:02 +0800

    Added adjustment for lightcurve for IPS source

--- mwa_ips/bin_it_up.py
+++ mwa_ips/bin_it_up.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 on = np.loadtxt('ts_on_source.txt')
 off = np.loadtxt('ts_off_source.txt')
 
+mean_on = np.mean(on)
+mean_off = np.mean(off)
+
+# From Angie:
+# On source: pixel coordinates 1546, 1258, value of 1.07391
+# off source: pixel coordinates 1549, 1563, value of 0.00331
+
+mean_on_should_be = 1.07391
+mean_off_should_be = 0.00331
+
+mean_on_adjust = mean_on_should_be - mean_on
+mean_off_adjust = mean_off_should_be - mean_off
+
+on += mean_on_adjust
+off += mean_off_adjust
+
 b = 20 # Number of time samples to bin together
 sample_time = 0.5 # seconds
 
@@ -12,10 +28,11 @@ binned_off = np.mean(np.reshape(off, (-1, b)), axis=-1)
 
 yerr = np.std(off)/np.sqrt(b)
 
-plt.errorbar(np.arange(b), binned_on, yerr=yerr, label="On")
-plt.errorbar(np.arange(b), binned_off, yerr=yerr, label="Off");
+t = np.arange(b) * sample_time * b
+plt.errorbar(t, binned_on, yerr=yerr, label="On")
+plt.errorbar(t, binned_off, yerr=yerr, label="Off");
 
-plt.xlabel("Time bin")
+plt.xlabel("Time (s)")
 plt.ylabel("Flux density (Jy/beam)")
 
 plt.legend()
