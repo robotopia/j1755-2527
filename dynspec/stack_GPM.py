ec29d448169e179b69161c896288daacf05e3a6a
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2025-01-30 11:24:39 +0800

    A few more bells and whistles in stack_GPM.py

--- dynspec/stack_GPM.py
+++ dynspec/stack_GPM.py
@@ -8,10 +8,6 @@ import argparse
 
 def fold(times, period, pepoch):
 
-    # Dedisperse to infinite frequency
-    #D = 4.148808e3 * u.MHz**2 / u.pc * u.cm**3 * u.s
-    #times -= D * self.dm / freqs_MHz**2
-
     pulses_phases = ((times - pepoch) / period).decompose()
     pulses, phases = np.divmod(pulses_phases + 0.5, 1)
     phases -= 0.5
@@ -22,11 +18,31 @@ def barycentre(times, src_coord):
     bc_correction = times.light_travel_time(src_coord, ephemeris='jpl')
     return times + bc_correction
 
+def calc_dmdelay(dm, f, f_ref):
+    return 4.148808e3 * dm * (1/f**2 - 1/f_ref**2)
+
 def StokesI_ds(dat):
     if dat['POLS'][0] == 'XX' and dat['POLS'][3] == 'YY':
         return 0.5*np.real(dat['DS'][:,:,0] + dat['DS'][:,:,3])
     else:
         raise NotImplementedError(f"I haven't been taught how to deal with POLS = {ds['POLS']} yet")
+
+def dedisperse_ds(ds, dm, f, f_ref, bin_size_seconds):
+    dmdelays = calc_dmdelay(dm, f, f_ref)
+
+    # It doesn't matter if the edges are wrapped, so use simple FFT to do
+    # the DM delay adjustment (essentially, a "roll" with fractional bins)
+    FFT = np.fft.rfft(ds, axis=0)
+    FFT_freqs = np.fft.rfftfreq(ds.shape[0], d=bin_size_seconds)
+
+    # Make a mesh grid of delays and frequencies with the same size as the FFT matrix
+    DMDELAYS, FFT_FREQS = np.meshgrid(dmdelays, FFT_freqs)
+
+    # Apply the phase ramp
+    FFT *= np.exp(2j*np.pi*FFT_FREQS*DMDELAYS)
+
+    # Invert the FFT
+    return np.fft.irfft(FFT, n=ds.shape[0], axis=0)
 
 def main():
     parser = argparse.ArgumentParser(description="Stack dynamic spectra together according to a folding ephemeris")
@@ -34,7 +50,7 @@ def main():
     parser.add_argument('coord', help="RA/Dec of source, in the format \"HH:MM:SS.S ±DD:MM:SS.S\"")
     parser.add_argument('period', type=float, help="The period of the source, in seconds")
     parser.add_argument('PEPOCH', type=float, help="A reference epoch (MJD) to mark zero rotation phase")
-    parser.add_argument('DM', type=float, help="The dispersion measure, in pc/cm³")
+    parser.add_argument('--DM', type=float, help="The dispersion measure, in pc/cm³")
 
     parser.add_argument('bin_size', type=float, help="The size of a time bin, in seconds")
     parser.add_argument('--fscrunch_factor', type=int, help="How many frequency channels to average together in the final output plot")
@@ -50,7 +66,8 @@ def main():
     src_coord = SkyCoord(args.coord, unit=(u.hourangle, u.deg), frame='icrs')
     src_period = args.period * u.s
     src_pepoch = Time(args.PEPOCH, scale='utc', format='mjd')
-    src_dm = args.DM * u.pc / u.cm**3
+    if args.DM is not None:
+        src_dm = args.DM * u.pc / u.cm**3
 
     # Binning parameters
     bin_size = args.bin_size * u.s
@@ -91,6 +108,8 @@ def main():
         dat['PHASE_BINS'] = phase_bins
         dats.append(dat)
 
+    f = dats[0]['FREQS']/1e6 # MHz
+    
     # Work out the dimensions of the final output array
     nphase_bins = max_phase_bin - min_phase_bin + 1
     nchans = len(dats[0]['FREQS'])
@@ -124,11 +143,17 @@ def main():
         #print(phase_bins)
 
     mean_ds = output_ds / counts
-    profile = np.nanmean(mean_ds, axis=1)
+    t = bin_size.to('s').value * (np.arange(nphase_bins) + min_phase_bin)
 
-    # Draw a curve representing the DM
-    f = dats[0]['FREQS']/1e6 # MHz
-    dmdelay = 4.148808e3 * src_dm.to('pc cm-3').value * (1/f**2 - 1/np.mean(f)**2)
+    # If a DM has been provided, dedisperse the composite dynamic spectrum
+    if args.DM is not None:
+        f_ref = np.nanmean(f)
+        mean_ds = dedisperse_ds(mean_ds, args.DM, f, f_ref, bin_size.to('s').value)
+        counts = dedisperse_ds(counts, args.DM, f, f_ref, bin_size.to('s').value)
+        t -= calc_dmdelay(args.DM, f_ref, np.inf)
+
+    # Form the profile
+    profile = np.nanmean(mean_ds, axis=1)
 
     # Do a bit of extra averaging in frequency
     if fscrunch_factor is not None:
@@ -147,16 +172,18 @@ def main():
     plt.close(fig)
 
     fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(5,8))
-    t = bin_size.to('s').value * (np.arange(nphase_bins) + min_phase_bin)
 
     axs[0].plot(t, profile)
     axs[1].pcolormesh(t, fscr, mean_ds.T, vmin=-0.1, vmax=0.6)
 
-    if args.draw_dm_sweep is not None:
+    # Draw a curve representing the DM
+    if args.DM is not None and args.draw_dm_sweep is not None:
         first = True
+        dmdelay = -calc_dmdelay(args.DM, f, np.nanmean(f))
+        # The minus sign ^^^ is because the dynamic spectrum will already be dedispersed
         for time in args.draw_dm_sweep:
-            label = f"DM = {src_dm.to('pc cm-3').value} pc/cm³" if first else None
-            axs[1].plot(dmdelay + time, f, 'r--', lw=2, label=label)
+            label = f"Zero DM curve" if first else None
+            axs[1].plot(dmdelay + time, f, 'w', lw=2, label=label)
             first = False
 
     cax = axs[2].pcolormesh(t, fscr, counts.T)
@@ -165,8 +192,10 @@ def main():
     axs[-1].set_xlabel("Time (s)")
     axs[0].set_ylabel("Flux density (Jy/beam)")
     axs[1].set_ylabel("Frequency (MHz)")
-    if args.draw_dm_sweep is not None:
+    if args.DM is not None and args.draw_dm_sweep is not None:
         axs[1].legend()
+    if args.DM is not None:
+        axs[0].set_title(f"Dedispersed to DM = {args.DM} pc/cm³")
     axs[2].set_ylabel("Frequency (MHz)")
 
     plt.tight_layout()
