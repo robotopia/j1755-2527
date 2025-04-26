e46f46b420049164bd949016eb4a8aed6a7946f5
Sammy McSweeney <sammy.mcsweeney@gmail.com>
2025-04-26 13:16:14 +0800

    Included DM fitting into ephemeris fitting script as well

--- dynspec/ephemeris_fits.py
+++ dynspec/ephemeris_fits.py
@@ -7,6 +7,7 @@ from timing import *
 
 table = QTable.read('toas.ecsv', format='ascii.ecsv')
 ephemeris = get_J1755_ephemeris()
+freqs = table['freq']
 
 def period_model(toas_mjd, period_d, pepoch_mjd):
     _, phases = np.divmod((toas_mjd - pepoch_mjd)/period_d + 0.5, 1)
@@ -30,8 +31,8 @@ p0 = [ephemeris['period'].to('d').value,
 popt, pcov = curve_fit(period_model, toas_mjd, np.zeros(toas_mjd.shape), p0=p0)
 errs = np.sqrt(np.diag(pcov))
 print("Period-only model:")
-print(f'P            = {popt[0]*86400:.5f} ± {errs[0]*86400:.5f} s')
-print(f'PEPOCH (MJD) = {popt[1]:.6f} ± {errs[1]:.6f}')
+print(f'  P            = {popt[0]*86400:.5f} ± {errs[0]*86400:.5f} s')
+print(f'  PEPOCH (MJD) = {popt[1]:.6f} ± {errs[1]:.6f}')
 print()
 
 def period_and_pdot_model(toas_mjd, period_d, pepoch_mjd, fdot):
@@ -44,8 +45,25 @@ popt, pcov = curve_fit(period_and_pdot_model, toas_mjd, np.zeros(toas_mjd.shape)
 errs = np.sqrt(np.diag(pcov))
 
 print("Period and Pdot model:")
-print(f'P            = {popt[0]*86400} ± {errs[0]*86400} s')
-print(f'Fdot         = {popt[2]} ± {errs[2]} / day²')
-print(f'PEPOCH (MJD) = {popt[1]} ± {errs[1]}')
+print(f'  P            = {popt[0]*86400:.5f} ± {errs[0]*86400:.5f} s')
+print(f'  Fdot         = {popt[2]} ± {errs[2]} / day²')
+print(f'  PEPOCH (MJD) = {popt[1]:.6f} ± {errs[1]:.6f}')
+print()
+
+def period_and_dm_model(toas_mjd_and_freqs, period_d, pepoch_mjd, dm):
+    toas_mjd, freqs = toas_mjd_and_freqs.T
+    t = toas_mjd - pepoch_mjd - calc_dmdelay(dm*u.pc/u.cm**3 - ephemeris['DM'], freqs*u.MHz, np.inf*u.MHz).to('d').value
+    _, phases = np.divmod(t/period_d + 0.5, 1)
+    return phases - 0.5
+
+p0[2] = 710
+toas_mjd_and_freqs = np.array([toas_mjd, freqs.to('MHz').value]).T
+popt, pcov = curve_fit(period_and_dm_model, toas_mjd_and_freqs, np.zeros(toas_mjd.shape), p0=p0)
+errs = np.sqrt(np.diag(pcov))
+
+print("Period and DM model:")
+print(f'  P            = {popt[0]*86400:.5f} ± {errs[0]*86400:.5f} s')
+print(f'  PEPOCH (MJD) = {popt[1]:.6f} ± {errs[1]:.6f}')
+print(f'  DM           = {popt[2]:.0f} ± {errs[2]:.0f} pc/cm³')
 print()
 
