Fixed normalisation, made pretty plot

meerkat/pb_correct.py
@@ -17,7 +17,8 @@ def gaussian_1d(x, mu, sigma):
   Returns:
     ndarray: An array of the same shape as x, containing the Gaussian values.
   """
-  return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2)
+  #return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2)
+  return np.exp(-0.5 * ((x - mu) / sigma)**2)
 
 c = 3.e8*u.m/u.s
 D = 13.5*u.m
@@ -28,8 +29,24 @@ nu = sys.argv[3]*u.MHz #MHz
 
 # Calibrated to the plots of Villiers 2023
 fwhm = 1.13*np.degrees(((c/nu) / D)*u.rad)
-theta = fwhm/2.355
+print(f"Beam FWHM = {fwhm}")
+sigma = fwhm/2.355
+print(f"Beam sigma = {sigma}")
 
 sep = coords_1.separation(coords_2)
+print(f"Distance from pointing centre = {sep.deg} deg")
 
-print(gaussian_1d(sep.deg, 0, theta.value))
+print(gaussian_1d(sep.deg, 0, sigma.value))
+
+from matplotlib import pyplot as plt
+
+x = np.linspace(0, 1.5*fwhm.value)
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111)
+ax.plot(x, gaussian_1d(x, 0, sigma.value), label='primary beam', color='black')
+ax.scatter(sep.deg, gaussian_1d(sep.deg, 0, sigma.value), label='source position', color='red')
+ax.set_xlabel("Distance from boresight / deg")
+ax.set_ylabel("Primary beam value (normalised to 1.0)")
+ax.set_title(f"Primary beam at {nu}")
+ax.legend()
+fig.savefig("Primary beam correction.png", bbox_inches="tight")