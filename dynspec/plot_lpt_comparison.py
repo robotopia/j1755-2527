68e71901b0367001b179afc9d8ca85fb91be20fd
Sam McSweeney <sammy.mcsweeney@gmail.com>
2025-05-17 10:49:23 +0800

    Working in LPT comparison plot for discussion

--- dynspec/plot_lpt_comparison.py
+++ dynspec/plot_lpt_comparison.py
@@ -1,67 +1,230 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib.legend_handler import HandlerTuple
 import astropy.units as u
+
+point_types = {
+    'Polars': {
+        'color': 'r',
+        'ecolor': 'r',
+        'capsize': 2,
+    },
+    'Unknown': {
+        'color': 'k',
+        'ecolor': 'k',
+        'capsize': 2,
+    },
+    'AR Sco-like': {
+        'color': 'b',
+        'ecolor': 'b',
+        'capsize': 2,
+    },
+}
 
 lpts = [
     {
+        'name': 'ASKAP J1755-2527',
+        'type': 'Unknown',
+        'min_width': 40 * u.s, # Be sure to note: these are W10, unscattered
+        'max_width': 172 * u.s,
+        'period': 4186.32827 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        },
+    },
+    {
         'name': 'ASKAP J1832-0911',
-        'min_width': 0 * u.s,  # TODO
-        'max_width': 0 * u.s,  # TODO
+        'type': 'Unknown',
+        'min_width': 2 * u.min,
+        'max_width': 4 * u.min,
         'period': 2656.24 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
     {
         'name': 'ASKAP J1839-0756',
+        'type': 'Unknown',
         'min_width': 320 * u.s,
         'max_width': 710 * u.s,
         'period': 23221.29 * u.s,
+        'text_kwargs': {
+            'ha': 'right',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        },
+        'voffset': 1,
     },
     {
         'name': 'ASKAP J1935+2148',
+        'type': 'Unknown',
         'min_width': 10 * u.s,
         'max_width': 50 * u.s,
         'period': 3225.313 * u.s,
+        'text_kwargs': {
+            'ha': 'right',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
     {
         'name': 'CHIME J0630+25',
+        'type': 'Unknown',
         'min_width': 0.2 * u.s,
         'max_width': 0.91 * u.s,
         'period': 421.35542 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
     {
         'name': 'GCRT J1745-3009',
+        'type': 'Unknown',
         'min_width': 600 * u.s,
         'max_width': 600 * u.s,
         'period': 4620.720 * u.s,  # Remember to cite Spreeuw et al. (2009) for this number.
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'x',
+            'ls': 'none',
+        }
+    },
+    {
+        'name': 'GLEAM-X J0704-37',
+        'type': 'Polars',
+        'min_width': 30 * u.s,
+        'max_width': 80 * u.s,
+        'period': 10496.6 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
     {
         'name': 'GLEAM-X J1627-5235',
+        'type': 'Unknown',
         'min_width': 30 * u.s,
         'max_width': 60 * u.s,
         'period': 1091.169 * u.s,
+        'text_kwargs': {
+            'ha': 'right',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        },
+        'voffset': 1,
     },
     {
         'name': 'GPM J1839-10',
+        'type': 'Unknown',
         'min_width': 30 * u.s,
         'max_width': 300 * u.s,
         'period': 1318.1957 * u.s,
+        'text_kwargs': {
+            'ha': 'right',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
     {
         'name': 'ILT J1101+5521',
+        'type': 'Polars',
         'min_width': 30 * u.s,
         'max_width': 90 * u.s,
         'period': 7531.17 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
+    },
+    {
+        'name': 'J1912-4410',
+        'type': 'AR Sco-like',
+        'min_width': 10 * u.s, # TODO: Get Csanad to give me better numbers from data
+        'max_width': 20 * u.s,
+        'period': 319.34903 * u.s,
+        'text_kwargs': {
+            'ha': 'left',
+            'va': 'center',
+        },
+        'errorbar_kwargs': {
+            'fmt': 'none',
+            'ls': 'none',
+        }
     },
 ]
 
-periods = [lpt['period'].to('s').value for lpt in lpts] * u.s
-min_duty_cycles = np.array([(lpt['min_width']/lpt['period']).decompose() for lpt in lpts])
-max_duty_cycles = np.array([(lpt['max_width']/lpt['period']).decompose() for lpt in lpts])
-duty_cycles = 0.5*(max_duty_cycles + min_duty_cycles)
-duty_cycles_err = 0.5*(max_duty_cycles - min_duty_cycles)
+period_units = 'min'
 
-plt.errorbar(periods.to('min').value, duty_cycles, yerr=duty_cycles_err, capsize=2, fmt='none', ls='none')
+plt.figure(figsize=(8, 3))
+
+for lpt in lpts:
+    min_duty_cycle = (lpt['min_width']/lpt['period']).decompose()
+    max_duty_cycle = (lpt['max_width']/lpt['period']).decompose()
+    duty_cycle = 0.5*(max_duty_cycle + min_duty_cycle)
+    duty_cycle_err = 0.5*(max_duty_cycle - min_duty_cycle)
+
+    plt.errorbar(
+        lpt['period'].to(period_units).value,
+        duty_cycle,
+        yerr=duty_cycle_err,
+        **lpt['errorbar_kwargs'],
+        **point_types[lpt['type']],
+    )
+
+    lpt_label = " " + lpt['name'] if lpt['text_kwargs']['ha'] == 'left' else lpt['name'] + " "
+    if 'voffset' in lpt.keys():
+        lpt_label = '\n'*lpt['voffset'] + lpt_label
+    plt.annotate(lpt_label, (lpt['period'].to(period_units).value, duty_cycle + 0.5*duty_cycle_err), **lpt['text_kwargs'], alpha=0.5)
+
+# Construct custom legend
+custom_lines = []
+for point_type, kwargs in point_types.items():
+    custom_lines.append(mlines.Line2D([], [], color=kwargs['color'], marker='_', linestyle='-', label=point_type))
+plt.legend(handles=custom_lines, loc='lower right')
+
 plt.xscale('log')
 plt.yscale('log')
-plt.xlabel("Period (min)")
+plt.xlabel(f"Period ({period_units})")
 plt.ylabel("Duty cycle")
-plt.show()
+
+plt.tight_layout()
+plt.savefig('lpt_comparison.pdf')
